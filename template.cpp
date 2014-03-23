566e5656bcbaa777434535bec0ce3759dba641ab
Robert <robert@ubuntu.(none)>
2014-03-23 01:54:54 -0700

    Add code template.

---
+++ template.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <cassert>
+
+using namespace std;
+
+resovle() {
+}
+
+int main() {
+  int T;
+  cin >> T;
+  for (int t = 0; t < T; t++) {
+
+    cout << "Case #" << t + 1 << ": " << resolve() << endl;
+  }
+  return 0;
+}
+

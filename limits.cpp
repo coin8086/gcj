571e5a97e600028a52f0ff75fd77e17854b2e05e
Robert Zhang <louirobert@gmail.com>
2014-04-05 11:06:04 +0800

    Add a tool for printing numeric limits.

---
+++ limits.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <climits>
+#include <cfloat>
+
+using namespace std;
+
+int main() {
+  cout << "LLONG_MIN: " << LLONG_MIN << endl;
+  cout << "LLONG_MAX: " << LLONG_MAX << endl;
+  cout << "ULLONG_MAX: " << ULLONG_MAX << endl;
+  cout << "DBL_MANT_DIG: " << DBL_MANT_DIG << endl;
+  cout << "LDBL_MANT_DIG: " << LDBL_MANT_DIG << endl;
+  cout << "DBL_DIG: " << DBL_DIG << endl;
+  cout << "LDBL_DIG: " << LDBL_DIG << endl;
+  cout << "DBL_MAX: " << DBL_MAX << endl;
+  cout << "DBL_MIN: " << DBL_MIN << endl;
+  cout << "LDBL_MAX: " << LDBL_MAX << endl;
+  cout << "LDBL_MIN: " << LDBL_MIN << endl;
+  return 0;
+}
+

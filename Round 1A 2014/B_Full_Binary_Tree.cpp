58fc23327c3c904ad3284353e466b4b1ff1d684c
Robert <louirobert@gmail.com>
2014-05-01 21:14:33 -0700

    Add comments and rename a function for Full Binary Tree.

--- Round 1A 2014/B_Full_Binary_Tree.cpp
+++ Round 1A 2014/B_Full_Binary_Tree.cpp
@@ -15,6 +15,7 @@ inline vector<int> get_children(const Graph & g, int node, int parent) {
   return children;
 }
 
+//Tell how many nodes to remove to delete a subtree rooted at node.
 int del(const Graph & g, int node, int parent) {
   assert(node > 0 && parent > 0);
   int size = g[node].size() - 1;
@@ -33,8 +34,12 @@ bool cmp(const pair<int, int> & a, const pair<int, int> & b) {
   return a.second < b.second;
 }
 
-inline int optimize(const vector<pair<int, int> > & trees) {
+//Select two children so that there is minimum number of nodes to remove.
+inline int select(const vector<pair<int, int> > & trees) {
   int m = INT_MAX;
+  //For each pair of children i, j, suppose we keep them and remove others.
+  //We then calcualte the number of nodes to remove. Among all candidate pairs,
+  //we choose the minimum.
   for (int i = 0; i < trees.size() - 1; i++) {
     for (int j = i + 1; j < trees.size(); j++) {
       int v = 0;
@@ -51,6 +56,7 @@ inline int optimize(const vector<pair<int, int> > & trees) {
   return m;
 }
 
+//Tell how many nodes to remove to make a subtree rooted at node become a full binary tree.
 int trim(const Graph & g, int node, int parent = 0) {
   assert(node > 0);
   int size = g[node].size();
@@ -64,15 +70,18 @@ int trim(const Graph & g, int node, int parent = 0) {
     return del(g, children[0], node);
   if (size == 2)
     return trim(g, children[0], node) + trim(g, children[1], node);
+  //For each child, we compute the trim value and del value so that we can determine
+  //the minimum nodes to remove.
   vector<pair<int, int> > trees;
   for (int i = 0; i < children.size(); i++) {
     trees.push_back(make_pair(trim(g, children[i], node), del(g, children[i], node)));
   }
-  return optimize(trees);
+  return select(trees);
 }
 
 inline int resolve(const Graph & g) {
   int m = INT_MAX;
+  //Try each node as root, then get the minimum.
   for (int i = 1; i < g.size(); i++) {
     int r = trim(g, i);
     if (r < m) {
